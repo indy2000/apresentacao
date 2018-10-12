4f493dfd1112545e4bea31c844b3adfa5c2a24a7
Gabriel Xavier <p.set57@gmail.com>
2018-10-12 19:14:51 -0300

    atualizando a tela

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,19 +4,20 @@ class ClientsController < ApplicationController
 	end
 
 	def create
-		valores = params.require(:client).permit :capsula, :creme, :sache, :shampoo,:floral,:homeopatia
-		clientes = Client.new valores
+		valores = params.require(:client).permit :nome,:capsula, :creme, :sache, :shampoo,:floral,:homeopatia
+		$clientes = Client.new valores
 
-		p "chamando a funçao create"
-		@@videos_escolhidos = clientes.escolhaVideo(valores[:capsula].to_i.to_b?,valores[:creme].to_i.to_b?,valores[:sache].to_i.to_b?,valores[:shampoo].to_i.to_b?,valores[:floral].to_i.to_b?,valores[:homeopatia].to_i.to_b?)
-		p @@videos_escolhidos
 
+		$videos_escolhidos = $clientes.escolhaVideo(valores[:capsula].to_i.to_b?,valores[:creme].to_i.to_b?,valores[:sache].to_i.to_b?,valores[:shampoo].to_i.to_b?,valores[:floral].to_i.to_b?,valores[:homeopatia].to_i.to_b?)
+		
 		respond_to do |format|
 			format.html {redirect_to :action =>"show"}
+			
 		end
 	end
 	def show
 		p "metodo show"
-		p @@videos_escolhidos
+		p $videos_escolhidos
+		p $clientes.nome
 	end
 end
